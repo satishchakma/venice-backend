45cc383905e6d76ee839027196943d86a6026ceb
Kaiser <ahmedkaiser.001@gmail.com>
2023-08-17 23:30:37 +0600

    add a new line in app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 
-app.use(cors());
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
