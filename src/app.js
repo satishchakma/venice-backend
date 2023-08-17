15e71fdeba10444e5d111396df5dcb16dd1ce46f
satish chakma <satishchakma.gd@gmail.com>
2023-08-17 23:43:31 +0600

    pulling new files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-module.exports = app
+module.exports = app;
