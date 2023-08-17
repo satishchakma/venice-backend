9132636c9d5865f59a029d6358ad9c6cd71acfdd
satish chakma <satishchakma.gd@gmail.com>
2023-08-17 23:51:40 +0600

    added another comment

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+//hello from the other side
+//hello from inside
 
 module.exports = app;
